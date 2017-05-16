0c5a6b85e66568fd938e1cadb8e6148ecc67d450
ThiagoAntonioFerreira <thiago.antonio.ferreira@gmail.com>
2017-05-15 22:50:02 -0300

    Subindo arquivos.

--- WindowsFormsApplication3/Program.cs
+++ WindowsFormsApplication3/Program.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApplication3
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main() 
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
