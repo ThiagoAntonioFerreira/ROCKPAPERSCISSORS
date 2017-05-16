b4ed04fd370607b525e6fb9b937de9b5b6704e65
ThiagoAntonioFerreira <thiago.antonio.ferreira@gmail.com>
2017-05-15 23:03:22 -0300

    Commit.

--- WindowsFormsApplication3/FrmMain.cs
+++ WindowsFormsApplication3/FrmMain.cs
@@ -13,7 +13,7 @@ namespace WindowsFormsApplication3
      
     public partial class Form1 : Form
     {
-        public object[] game;
+        public object[] jogo;
         public Form1()
         {
             InitializeComponent();
@@ -21,53 +21,101 @@ namespace WindowsFormsApplication3
 
         private void button1_Click(object sender, EventArgs e)
         {
+            /**Exemplos....**/
+
             /*
              [ ["Armando", "P"], ["Dave", "S"] ]
              */
-            game = new object[2];
-            game[0] = new Player("Armando", "P");
-            game[1] = new Player("Dave", "S");
+            jogo = new object[2];
+            jogo[0] = new Player("Armando", "P");
+            jogo[1] = new Player("Dave", "S");
 
-            rps_game_winner(game);
-
+            object winner = rps_tournament_winner(jogo);
+            textBox1.Text += "Vencedor....:";
+            textBox1.Text += (winner as Player).jogador + " ";
+            textBox1.Text += (winner as Player).jogada;
 
             /*
              [ ["Armando", "P"], ["Dave", "S"] ],
              [ ["Richard", "R"], ["Michael", "S"] ],
              */
-            object[] game1 = new object[2];
+            object[] jogo1 = new object[2];
 
-            game = new object[2];
-            game[0] = new Player("Armando", "P");
-            game[1] = new Player("Dave", "S");
-            game1[0] = game;
+            jogo = new object[2];
+            jogo[0] = new Player("Armando", "P");
+            jogo[1] = new Player("Dave", "S");
+            jogo1[0] = jogo;
 
-            game = new object[2];
-            game[0] = new Player("Richard", "R");
-            game[1] = new Player("Michel", "S");
-            game1[1] = game;
+            jogo = new object[2];
+            jogo[0] = new Player("Richard", "R");
+            jogo[1] = new Player("Michel", "S");
+            jogo1[1] = jogo;
 
 
-            object winner = rps_tournament_winner(game1);
-
-            textBox1.Text += "Vencedor....:";
-            textBox1.Text += (winner as Player).jogador;
+            winner = rps_tournament_winner(jogo1);
+            textBox1.Text += Environment.NewLine;
+            textBox1.Text += "Vencedor Jogo 1....:";
+            textBox1.Text += (winner as Player).jogador + " ";
             textBox1.Text += (winner as Player).jogada;
+
+
+            /*
+             [
+                    [
+                        [ ["Armando", "R"], ["Dave", "S"] ],
+                        [ ["Richard", "S"],  ["Michael", "S"] ],
+                    ],
+                    [
+                        [ ["Allen", "P"], ["Omer", "S"] ],
+                        [ ["David", "R"], ["Richard", "S"] ]
+                    ]
+                ]
+             */
+            object[] jogo2 = new object[2];
+
+            jogo = new object[2];
+            jogo[0] = new Player("João", "R");
+            jogo[1] = new Player("Jose", "S");
+            jogo2[0] = jogo;
+
+            jogo = new object[2];
+            jogo[0] = new Player("Cleber", "S");
+            jogo[1] = new Player("Maria", "S");
+            jogo2[1] = jogo;
+
+            jogo = new object[2];
+            jogo[0] = new Player("Michel", "P");
+            jogo[1] = new Player("Joana", "S");
+            jogo2[0] = jogo;
+
+            jogo = new object[2];
+            jogo[0] = new Player("Daniel", "R");
+            jogo[1] = new Player("Ricardo", "S");
+            jogo2[1] = jogo;
+
+
+            winner = rps_tournament_winner(jogo2);
+            textBox1.Text += Environment.NewLine;
+            textBox1.Text += "Vencedor Jogo 2....:";
+            textBox1.Text += (winner as Player).jogador + " ";
+            textBox1.Text += (winner as Player).jogada;
+
+
         }
 
         public object rps_game_winner(object[] game)
         {
             if (game.Length > 2)
-                throw new Exception("");
+                throw new WrongNumberOfPlayersError("Numero de Jogadores inválido");
 
             Player p1 = game[0] as Player;
             Player p2 = game[1] as Player;
 
             if (p1.jogada.ToUpper().IndexOfAny(new char[] { 'R', 'S', 'P' }) == -1)
-                throw new Exception("");
+                throw new NoSuchStrategyError("Estratégia inválida");
 
             if (p2.jogada.ToUpper().IndexOfAny(new char[] { 'R', 'S', 'P' }) == -1)
-                throw new Exception("");
+                throw new NoSuchStrategyError("Estratégia inválida");
 
             if (p1.jogada == "R" && p2.jogada == "S")
                 return p1;
@@ -115,6 +163,32 @@ namespace WindowsFormsApplication3
             }
         }
 
+
+        public class NoSuchStrategyError : Exception
+        {
+            public NoSuchStrategyError()
+            {
+            }
+
+            public NoSuchStrategyError(string message)
+                : base(message)
+            {
+            }
+
+        }
+
+        public class WrongNumberOfPlayersError : Exception
+        {
+            public WrongNumberOfPlayersError()
+            {
+            }
+
+            public WrongNumberOfPlayersError(string message)
+                : base(message)
+            {
+            }
+
+        }
     }
     
 }
